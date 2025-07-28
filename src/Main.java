57961734deeb28bf941d5afefc08e39f21b99715
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2025-07-28 22:39:49 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        Student student1 = new Student("krishna", 46, 'A');
-        student1.displayDetails();
-    }
-}
-
 class Student {
-    String name;
-    int rollNumber;
-    char grade;
+     String name;
+     int rollNumber;
+     char grade;
 
-    
-    public Student(String name, int rollNumber, char grade) {
-        this.name = name;
-        this.rollNumber = rollNumber;
-        this.grade = grade;
-    }
-
-    
-    public void displayDetails() {
-        System.out.println("Name: " + name);
-        System.out.println("Roll Number: " + rollNumber);
-        System.out.println("Grade: " + grade);
+     public void displayDetails() {
+             System.out.println("Name: " + name);
+             System.out.println("Roll Number: " + rollNumber);
+             System.out.println("Grade: " + grade);
+     }
+}
+public class Main {
+      public static void main(String[] args) {
+        Student student = new Student();
+           student.name = "Krishna";
+           student.rollNumber = 46;
+           student.grade = 'A';
+           
+           student.displayDetails();
     }
 }
